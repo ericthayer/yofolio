71503630df7a9c1c024de85b1fb5b37f20a68fb1
Eric Thayer <eric.thayer594@gmail.com>
2026-03-01 16:12:31 -0700

    Updated Hero.tsx

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -250,6 +250,20 @@ export const HeroAction: React.FC<HeroActionProps> = ({
   );
 };
 
+// Hero Divider
+export interface HeroDividerProps extends BoxProps {
+  sx: Sx<Theme>;
+}
+
+export const HeroDivider: React.FC<HeroDividerProps> = ({
+  sx
+  ...rest
+}) => {
+  return (
+    <Box sx={{ background: url('data:image/svg+xml,<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1000 100"><g fill="%23000"><path d="M0 1v99c134.3 0 153.7-99 296-99H0Z" opacity=".5"></path><path d="M1000 4v86C833.3 90 833.3 3.6 666.7 3.6S500 90 333.3 90 166.7 4 0 4h1000Z" opacity=".5"></path><path d="M617 1v86C372 119 384 1 196 1h421Z" opacity=".5"></path><path d="M1000 0H0v52C62.5 28 125 4 250 4c250 0 250 96 500 96 125 0 187.5-24 250-48V0Z"></path></g></svg>'), height: '10vh' }} />
+  )
+}
+
 // Compound Component Export
 Hero.Container = HeroContainer;
 Hero.Content = HeroContent;
